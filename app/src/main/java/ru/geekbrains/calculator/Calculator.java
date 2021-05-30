38db3fb0020b9bebf402ba1ff3f5f56de6ecfe83
PivIgor <brauer.igor@gmail.com>
2021-05-30 20:29:27 +0300

    Homework 3 done.

---
+++ app/src/main/java/ru/geekbrains/calculator/Calculator.java
@@ -0,0 +1,195 @@
+package ru.geekbrains.calculator;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Calculator implements Parcelable {
+
+    private StringBuilder textScore;
+    private double[] args;
+    private int pointer;
+    private Operations currentOperation;
+    private boolean isEnteringNewArgument;
+
+    public Calculator() {
+        this.textScore = new StringBuilder("0");
+        this.args = new double[2];
+    }
+
+    protected Calculator(Parcel in) {
+        this.args = in.createDoubleArray();
+        this.pointer = in.readInt();
+        this.isEnteringNewArgument = in.readByte() != 0;
+        this.textScore = new StringBuilder(in.readString());
+        this.currentOperation = (Operations) in.readSerializable();
+    }
+
+    public static final Creator<Calculator> CREATOR = new Creator<Calculator>() {
+        @Override
+        public Calculator createFromParcel(Parcel in) {
+            return new Calculator(in);
+        }
+
+        @Override
+        public Calculator[] newArray(int size) {
+            return new Calculator[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeDoubleArray(args);
+        dest.writeInt(pointer);
+        dest.writeByte((byte) (isEnteringNewArgument ? 1 : 0));
+        dest.writeString(textScore.toString());
+        dest.writeSerializable(currentOperation);
+    }
+
+    public String getTextScore() {
+        return this.textScore.toString();
+    }
+
+    public void put(char ch) {
+        if (!isEnteringNewArgument) {
+            textScore = new StringBuilder("0");
+            isEnteringNewArgument = true;
+        }
+        if (!validateAppendChar(ch)) {
+            return;
+        }
+        this.textScore.append(ch);
+        if (this.textScore.length() > 1
+                && this.textScore.charAt(0) == '0'
+                && this.textScore.charAt(1) != '.') {
+            this.textScore.deleteCharAt(0);
+        }
+    }
+
+    public void clear() {
+        args[0] = 0;
+        args[1] = 1;
+        pointer = 0;
+        currentOperation = null;
+        this.textScore = new StringBuilder("0");
+    }
+
+    public void del() {
+        char charToDel = textScore.charAt(textScore.length() - 1);
+        if ((charToDel < '0' || charToDel > '9')
+                && charToDel != '.') {
+            return;
+        }
+        textScore.deleteCharAt(textScore.length() - 1);
+        if (textScore.length() == 0
+                || (textScore.length() == 1 && textScore.charAt(0) == '-')) {
+            textScore = new StringBuilder("0");
+        }
+    }
+
+    public void plus() {
+        setOperation(Operations.PLUS);
+    }
+
+    public void minus() {
+        setOperation(Operations.MINUS);
+    }
+
+    public void div() {
+        setOperation(Operations.DIV);
+    }
+
+    public void mul() {
+        setOperation(Operations.MUL);
+    }
+
+    public void percent() {
+
+        if (currentOperation == null) {
+            clear();
+            return;
+        }
+
+        switch (currentOperation) {
+            case MINUS:
+            case PLUS:
+                setScoreNumber(args[0] * getScoreNumber() / 100);
+                break;
+            case DIV:
+            case MUL:
+                setScoreNumber(getScoreNumber() / 100);
+                break;
+        }
+    }
+
+    private void setScoreNumber(double number) {
+        textScore = new StringBuilder(Double.toString(number));
+    }
+
+    private double getScoreNumber() {
+        return Double.parseDouble(textScore.toString());
+    }
+
+    public void result() {
+        newArgumentApplied();
+        getNewResult();
+        pointer = 0;
+    }
+
+    private boolean validateAppendChar(char ch) {
+        if (ch == '.') {
+            for (int i = 0; i < textScore.length(); i++) {
+                if (textScore.charAt(i) == ch) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void setOperation(Operations op) {
+        if (newArgumentApplied()) {
+            getNewResult();
+        }
+        currentOperation = op;
+    }
+
+    private boolean newArgumentApplied() {
+        args[pointer] = getScoreNumber();
+        boolean isNewArgument = isEnteringNewArgument && pointer == 1;
+        isEnteringNewArgument = false;
+        if (pointer == 0) {
+            pointer = 1;
+        }
+        return isNewArgument;
+    }
+
+    private void getNewResult() {
+        if (currentOperation == null) {
+            return;
+        }
+        switch (currentOperation) {
+            case PLUS:
+                args[0] = args[0] + args[1];
+                break;
+            case MINUS:
+                args[0] = args[0] - args[1];
+                break;
+            case MUL:
+                args[0] = args[0] * args[1];
+                break;
+            case DIV:
+                args[0] = args[0] / args[1];
+                break;
+        }
+        setScoreNumber(args[0]);
+    }
+
+    private enum Operations {
+        PLUS, MINUS, DIV, MUL
+    }
+}
--- app/src/main/java/ru/geekbrains/calculator/MainActivity.java
+++ app/src/main/java/ru/geekbrains/calculator/MainActivity.java
@@ -1,14 +1,133 @@
 package ru.geekbrains.calculator;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private final String KEY_CALC = "calculator";
+    private Calculator calculator;
+    private TextView scoreboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            calculator = new Calculator();
+        } else {
+            calculator = savedInstanceState.getParcelable(KEY_CALC);
+        }
+        initOnClickListener();
+        scoreboard = findViewById(R.id.scoreboard);
+        showTextScore();
+    }
+
+    private void showTextScore() {
+        scoreboard.setText(calculator.getTextScore());
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(KEY_CALC, calculator);
+    }
+
+    private void initOnClickListener() {
+
+        initButtonOnClickListener(R.id.key_0);
+        initButtonOnClickListener(R.id.key_1);
+        initButtonOnClickListener(R.id.key_2);
+        initButtonOnClickListener(R.id.key_3);
+        initButtonOnClickListener(R.id.key_4);
+        initButtonOnClickListener(R.id.key_5);
+        initButtonOnClickListener(R.id.key_6);
+        initButtonOnClickListener(R.id.key_7);
+        initButtonOnClickListener(R.id.key_8);
+        initButtonOnClickListener(R.id.key_9);
+        initButtonOnClickListener(R.id.key_dot);
+        initButtonOnClickListener(R.id.key_backspace);
+        initButtonOnClickListener(R.id.key_c);
+        initButtonOnClickListener(R.id.key_div);
+        initButtonOnClickListener(R.id.key_mul);
+        initButtonOnClickListener(R.id.key_plus);
+        initButtonOnClickListener(R.id.key_minus);
+        initButtonOnClickListener(R.id.key_result);
+        initButtonOnClickListener(R.id.key_percent);
+    }
+
+    private void initButtonOnClickListener(int viewId) {
+        findViewById(viewId).setOnClickListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.key_0:
+                calculator.put('0');
+                break;
+            case R.id.key_1:
+                calculator.put('1');
+                break;
+            case R.id.key_2:
+                calculator.put('2');
+                break;
+            case R.id.key_3:
+                calculator.put('3');
+                break;
+            case R.id.key_4:
+                calculator.put('4');
+                break;
+            case R.id.key_5:
+                calculator.put('5');
+                break;
+            case R.id.key_6:
+                calculator.put('6');
+                break;
+            case R.id.key_7:
+                calculator.put('7');
+                break;
+            case R.id.key_8:
+                calculator.put('8');
+                break;
+            case R.id.key_9:
+                calculator.put('9');
+                break;
+            case R.id.key_dot:
+                calculator.put('.');
+                break;
+            case R.id.key_backspace:
+                calculator.del();
+                break;
+            case R.id.key_c:
+                calculator.clear();
+                break;
+            case R.id.key_plus:
+                calculator.plus();
+                break;
+            case R.id.key_minus:
+                calculator.minus();
+                break;
+            case R.id.key_div:
+                calculator.div();
+                break;
+            case R.id.key_mul:
+                calculator.mul();
+                break;
+            case R.id.key_percent:
+                calculator.percent();
+                break;
+            case R.id.key_result:
+                calculator.result();
+                break;
+        }
+        showTextScore();
     }
 }
